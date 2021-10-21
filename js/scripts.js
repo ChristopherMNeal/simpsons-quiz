d88313e34e81ab5b8dfc5d9d4568234e2fafcc99
Christopher Neal <christopher.m.neal@gmail.com>
2021-10-21 16:29:30 -0700

    Add UI logic
    Co-authored-by: Jeff Terrell <jeffaterrell@gmail.com>
    Co-authored-by: Aaron Minnick <abminnick@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
 //business logic
+let bart = 0;
+let lisa = 0;
+let nelson = 0;
+
 function addPoint(character) {
-  return character += 1;
+  if (character === "bart") {
+    bart += 1;
+  } else if (character === "lisa") {
+    lisa += 1;
+  } else if (character === "nelson") {
+    nelson += 1;
+  }
 }
 
-function winner(bart, nelson, lisa) {
-  if (bart === nelson || bart === lisa || nelson === lisa) {
-    return "Ralph";
-  } else if (bart > lisa && bart > nelson) {
+function winner(bart, lisa, nelson) {
+  if (bart > lisa && bart > nelson) {
     return "Bart";
   } else if (lisa > nelson && lisa > bart) {
     return "Lisa";
   } else if (nelson > bart && nelson > lisa) {
     return "Nelson"
+  } else if (bart === nelson || bart === lisa || nelson === lisa) {
+    return "Ralph";
   } else {
     return "Broken";
   }
@@ -22,3 +32,18 @@ function percentScore(character) {
 }
 
 // ui logic
+$(document).ready(function() {
+  $("#quiz").submit(function(event) {
+    event.preventDefault();
+
+    addPoint($("input:radio[name=hobby]:checked").val());
+    addPoint($("input:radio[name=school]:checked").val());
+    addPoint($("input:radio[name=food]:checked").val());
+    addPoint($("input:radio[name=phrase]:checked").val());
+
+    $("#result").text(winner(bart, lisa, nelson));
+    bart = 0;
+    lisa = 0;
+    nelson = 0;
+  });
+});
